1b34c06780e3af7344291e944cc12c6d8212d663
Nimesh Madhusanka <madhusankan19@gmail.com>
2026-02-13 22:20:54 +0530

    Initialize React with TypeScript frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Library Management System</h1>
+        <p>
+          Welcome to the Library Management System
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
